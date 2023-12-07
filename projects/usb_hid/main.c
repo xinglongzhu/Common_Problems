c99e1a3d576fe2be90bf0ef133e5e2c752c0a69c
xinglong <miracle.zhu@minew.com>
2023-12-07 17:49:51 +0800

    feat: source sdk17.1.0

---
+++ projects/usb_hid/main.c
@@ -0,0 +1,36 @@
+/* Copyright (C) XingHuoWuXian Technologies Co., Ltd
+   All rights reserved. */
+
+#define NRF_LOG_MODULE_NAME main
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+NRF_LOG_MODULE_REGISTER();
+
+/**
+ * @brief 空闲状态处理
+ */
+static void idle_state_handle(void)
+{
+    if (NRF_LOG_PROCESS() == false) {
+
+    }
+}
+
+/**
+ * @brief 初始化 log 模块
+ */
+static void log_init(void)
+{
+    APP_ERROR_CHECK(NRF_LOG_INIT(NULL));
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
+}
+
+int main(void)
+{
+   log_init();
+   NRF_LOG_INFO("main start\n");
+   for (;;) {
+      idle_state_handle();
+   }   
+}
